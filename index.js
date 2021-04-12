f27c685105acf1a156f8769dc13b6e3de9754ee3
Mike <mikewrubel51@gmail.com>
2021-04-12 16:16:09 -0400

    commit tree 8f05f191c042d26aa3ffa99354bc2782b25ac898

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+function superbowlWin(record){
 
+const found = record.find((i) => {
+    return i.result==='W';
+})
+
+if (found === undefined){
+    return undefined
+}
+else return found.year
+}
